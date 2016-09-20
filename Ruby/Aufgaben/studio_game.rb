692aedccf88e57340ca468ce2e9c434c0049c4bc
Seeker321 <kimtiedemann@gmx.net>
2016-09-20 12:55:15 +0200

    Ruby

---
+++ Ruby/Aufgaben/studio_game.rb
@@ -0,0 +1,12 @@
+name1 = "larry"
+health = 60
+puts "#{name1}'s health is #{health}"
+
+puts "#{name1}'s health is #{health * 3}"
+
+puts "#{name1}'s health is #{(health / 9.00).round(2)}"
+
+name2 = "curly"
+name3 = "moe"
+
+puts "\tPlayers:\n\t\t#{name1}\n\t\t#{name2}\n\t\t#{name3}"
--- Ruby/Unterricht/start.rb
+++ Ruby/Unterricht/start.rb
@@ -1,5 +1,17 @@
 comment = "Heute ist Dienstag"
 3.times do 
 	puts comment.upcase
-#Dies ist ein Kommentar
+
 end
+#---------------------
+var = "Mikey loves Goonies"
+puts 'Mickey\'s favorite movie is Goonies' # mit \ wird mit das Anführungszeichen maskiert
+
+movie = "Goonies"
+
+puts "Mickey's favorite movie is \n" + movie # \n = Zeilenumbruch, mit + Variablen anhängen
+
+rank = 10
+
+puts "#{movie} has a rank of #{rank} am #{Time.now}" # #{} gibt den Inhalt der Variable im String aus
+
